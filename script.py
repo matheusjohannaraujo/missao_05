2ae735029c69681f4698ed85c4fcdb302c16f77d
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2021-07-17 10:38:06 -0300

    Upd 4

--- script.py
+++ script.py
@@ -1,25 +1,73 @@
-
 """
-
 https://scikit-learn.org/stable/modules/svm.html
 https://scikit-learn.org/stable/modules/multiclass.html
 https://scikit-learn.org/stable/modules/preprocessing.html
 https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.RepeatedKFold.html
 https://archive.ics.uci.edu/ml/datasets/glass+identification
-
 """
 
-import matplotlib
 import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# Lendo dados
 dataset_df = pd.read_csv('glass.data')
 dataset_df.columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
 
+# Número de linhas e colunas
+print(f'dataset_df LxC = {dataset_df.shape}')
+
+# Limpando dados
+dataset_df = dataset_df.dropna()
+
+# Número de linhas e colunas
+print(f'dataset_df LxC = {dataset_df.shape}')
+
+# Embaralhando os dados
+dataset_df = dataset_df.sample(frac=1)
+#print(dataset_df)
+
+# Separação dos conjuntos de dados
+# Train = 60% | Temp Test = 40%
+train_dataset, temp_test_dataset = train_test_split(dataset_df, test_size=0.4)
+print(f'train_dataset LxC = {train_dataset.shape}')
+print(f'temp_test_dataset LxC = {temp_test_dataset.shape}')
+# Test = 50% | Valid  = 50%
+test_dataset, valid_dataset = train_test_split(temp_test_dataset, test_size=0.5)
+print(f'test_dataset LxC = {test_dataset.shape}')
+print(f'valid_dataset LxC = {valid_dataset.shape}')
+
+# Gráfico de Relações
+train_stats = train_dataset.describe()
+train_stats.pop('k')
+#sns.pairplot(train_stats[train_stats.columns], diag_kind='kde')
+#plt.show()
+train_stats = train_stats.transpose()
+#print(train_stats)
+
+# Definindo Labels
+train_labels = train_dataset.pop('k')
+test_labels = test_dataset.pop('k')
+valid_labels = valid_dataset.pop('k')
+
+# Normalização dos dados
+#def norm(x):
+#   return (x - train_stats['mean']) / train_stats['std']
+norm = lambda x: (x - train_stats['mean']) / train_stats['std']
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+normed_valid_data = norm(valid_dataset)
+
+normed_train_data.head(10)
+
+
+
+"""
 classes_sr = dataset_df['k']
 
 #print(classes_sr)
@@ -28,25 +76,4 @@ k = []
 for i in range(1, 8):
     k.append(dataset_df.loc[dataset_df['k'] == i])
 print(k)
-
-#scaler = preprocessing.StandardScaler().fit(classes_sr)
-
-#print(scaler)
-
-#print(dataset.loc[10])
-
-"""dataset2 = np.loadtxt('glass.data', delimiter=',')
-
-#print(dataset.head())
-
-#print(dataset)
-
-#print(dataset2[0])
-
-x = dataset2[:,0:7]
-y = dataset2[:,8]
-
-print(y)
-
-#print(dataset.loc[0])
 """
